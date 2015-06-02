a28b999608f638ace169aa21aa5c5ff6bd0d0ffa
Konstantin Koslowski <konstantin.koslowski@gmail.com>
2015-06-02 17:08:30 +0200

    moveplane: some cleanup

--- moveplane/move-plane.py
+++ moveplane/move-plane.py
@@ -7,16 +7,31 @@ import time
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 # initialize GPIOs
+# Motor1 step
 GPIO.setup("P8_8", GPIO.OUT)
+# Motor1 direction
 GPIO.setup("P8_10", GPIO.OUT)
+# Motor2 step
 GPIO.setup("P8_12", GPIO.OUT)
+# Motor2 direction
 GPIO.setup("P8_14", GPIO.OUT)
 
 # some variables
 prev_x = 0
 prev_y = 0
 prev_z = 0
-delta = 0.01
+delta = 0.02
+
+# single step on rising edge
+def step (pin):
+  GPIO.output(pin, GPIO.HIGH)
+  time.sleep(1E-6)
+  GPIO.output(pin, GPIO.LOW)
+
+# do some steps
+def loop (pin, num):
+  for i in range(0, num):
+    step(pin)
 
 # update plane position
 while True:
@@ -38,38 +53,33 @@ while True:
   x = round(x/m, 2)
   y = round(y/m, 2)
   z = round(z/m, 2)
-  print('x: %.2f y: %.2f z: %.2f' % (x, y, z))
+  # print('x: %.2f y: %.2f z: %.2f' % (x, y, z))
 
   # x axis
-  n = int((prev_x - x)/delta)
-  for i in range (0, n):
-    print("inc x")
-    GPIO.output("P8_10", GPIO.LOW)
-    GPIO.output("P8_8",  GPIO.HIGH)
-    time.sleep(1E-6)
-    GPIO.output("P8_8",  GPIO.LOW)
   n = int((x - prev_x)/delta)
-  for i in range (0, n):
-    print("dec x")
+  if n > 0:
+    print("inc x: %d" % (n))
+    GPIO.output("P8_10", GPIO.LOW)
+    loop("P8_8", n)
+  elif n < 0:
+    print("dec x: %d" % (abs(n)))
     GPIO.output("P8_10", GPIO.HIGH)
-    GPIO.output("P8_8",  GPIO.HIGH)
-    GPIO.output("P8_8",  GPIO.LOW)
+    loop("P8_8", abs(n))
 
   # y axis
-  n = int((prev_y - y)/delta)
-  for i in range (0, n):
-    print("inc y")
-    GPIO.output("P8_14", GPIO.LOW)
-    GPIO.output("P8_12", GPIO.HIGH)
-    GPIO.output("P8_12", GPIO.LOW)
   n = int((y - prev_y)/delta)
-  for i in range (0, n):
-    print("dec y")
+  if n > 0:
+    print("inc y: %d" % (n))
+    GPIO.output("P8_14", GPIO.LOW)
+    loop("P8_12", n)
+  elif n < 0:
+    print("dec y: %d" % (abs(n)))
     GPIO.output("P8_14", GPIO.HIGH)
-    GPIO.output("P8_12", GPIO.HIGH)
-    GPIO.output("P8_12", GPIO.LOW)
+    loop("P8_12", abs(n))
 
   prev_x = x
   prev_y = y
   prev_z = z
+
+  # wait for sensor data
   time.sleep(1E-3)
